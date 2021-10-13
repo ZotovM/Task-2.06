6a5a10b86c6686e57b48ee1495002a62e8929e7c
ZotovM <90614959+ZotovM@users.noreply.github.com>
2021-10-13 15:17:05 +0300

    Add files via upload

--- Equation.java
+++ Equation.java
@@ -1,4 +1,3 @@
-
 import java.util.Locale;
 import java.util.Scanner;
 public class Equation {
@@ -6,26 +5,28 @@ public class Equation {
         Locale.setDefault(Locale.ROOT);
 
         System.out.println("Система решает уравнение вида: ");
-        System.out.println("ax^2 + bx + c = 0" );
+        System.out.println("ax^2 + bx + c = 0");
         double a = readNumber("Введите значение а: ");
         double b = readNumber("Введите значение b: ");
         double c = readNumber("Введите значение с: ");
+        double D = calc(a, b, c);
+        answer(D);
+    }
+
+    private static double calc(double a, double b, double c) {
         double D = Math.pow(b, 2) - 4 * a * c;
-        if (D > 0) {
-            double x1 = -b + Math.sqrt(D) / (2 * a);
-            double x2 = -b - Math.sqrt(D) / (2 * a);
-            System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
-        }
-        else if (D == 0) {
-            double X = (-b + Math.sqrt(D)) / (2 * a);
-            double x = X;
-            System.out.println("Корень уравнения: x = " + x);
+        return D;
+    }
+
+    private static void answer(double D) {
+        if (D >= 0) {
+            System.out.println("Ответ:" + D);
         }
         else if (D < 0) {
-            System.out.println("Уранение корней не имеет!");
-            System.exit(1);
+            System.out.println("Ошибка! ");
         }
     }
+
 
     private static double readNumber(String text) {
         Scanner scanner = new Scanner(System.in);
